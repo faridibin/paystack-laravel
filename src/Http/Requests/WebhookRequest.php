2ae4806b10ad75d513c374f71492541ad359fd92
Farid Adam <faridibin@gmail.com>
2025-01-14 01:14:37 -0500

    Refactor WebhookController and WebhookRequest for improved middleware handling and validation response

--- src/Http/Requests/WebhookRequest.php
+++ src/Http/Requests/WebhookRequest.php
@@ -6,6 +6,7 @@ use Faridibin\Paystack\Enums\WebhookEvent;
 use Illuminate\Contracts\Validation\Validator;
 use Illuminate\Foundation\Http\FormRequest;
 use Illuminate\Http\Exceptions\HttpResponseException;
+use Illuminate\Http\Response;
 use Illuminate\Validation\Rule;
 
 class WebhookRequest extends FormRequest
@@ -41,10 +42,8 @@ class WebhookRequest extends FormRequest
      */
     protected function failedValidation(Validator $validator)
     {
-        throw new HttpResponseException(response()->json([
-            'status' => 'error',
-            'message' => 'Validation failed',
-            'errors' => $validator->errors()
-        ], 422));
+        $response = new Response('Validation failed:' . $validator->errors()->toJson(), 422);
+
+        throw new HttpResponseException($response);
     }
 }
