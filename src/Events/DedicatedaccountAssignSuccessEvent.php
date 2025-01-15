abcf764b04863f344bf022e303e63ae8c9e1985a
Farid Adam <faridibin@gmail.com>
2025-01-15 02:25:36 -0500

    Add event classes for various Paystack events

---
+++ src/Events/DedicatedaccountAssignSuccessEvent.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace Faridibin\PaystackLaravel\Events;
+
+use Illuminate\Foundation\Events\Dispatchable;
+use Illuminate\Queue\SerializesModels;
+
+class DedicatedaccountAssignSuccessEvent
+{
+    use Dispatchable, SerializesModels;
+
+    /**
+     * Create a new event instance.
+     */
+    public function __construct(
+        public array $data
+    ) {
+        //
+    }
+}
